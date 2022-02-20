a8e4ac4786abffc9469e4f991577e9e2c9760535
RomaYourchuk <yourchuk922@gmail.com>
2022-02-20 21:55:35 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,16 +2,16 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const localStorageKey = 'videoplayer-current-time';
-console.log(localStorageKey);
+// console.log(localStorageKey);
 
 const player = new Player('vimeo-player');
-console.log(player);
+// console.log(player);
 
 const key = localStorage.getItem(localStorageKey);
 if (key) {
   player.setCurrentTime(parseFloat(key));
 }
-console.log(key);
+// console.log(key);
 
 player.on(
   'timeupdate',
