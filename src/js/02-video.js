1189b86327f403b921789b27927c0cfc0f8a2cb8
RomaYourchuk <yourchuk922@gmail.com>
2022-02-20 19:41:21 +0200

    task 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const localStorageKey = 'videoplayer-current-time';
+console.log(localStorageKey);
+
+const player = new Player('vimeo-player');
+console.log(player);
+
+const key = localStorage.getItem(localStorageKey);
+if (key) {
+  player.setCurrentTime(parseFloat(key));
+}
+console.log(key);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(localStorageKey, data.seconds.toString());
+  }, 1000),
+);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,64 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector(".feedback-form")
+
+const localStorageKey = 'feedback-form-state';
+
+initForm()
+
+formEl.addEventListener('submit', event => {
+    event.preventDefault();
+    // console.log("submit")
+
+    const formData = new FormData(formEl);
+    // console.log(FormData)
+    formData.forEach((key, value) => console.log(`${value} - ${key}`));
+    localStorage.removeItem(localStorageKey);
+    formEl.reset();
+
+});
+
+
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+    // console.log('event.target.name', event.target.name);
+    // console.log('event.target.value', event.target.value);
+    let parsedFilters = localStorage.getItem(localStorageKey);
+    parsedFilters = parsedFilters ? JSON.parse(parsedFilters) : {};
+    // console.log(parsedFilters)
+
+    parsedFilters[event.target.name] = event.target.value;
+    // console.log(parsedFilters)
+
+    if(!parsedFilters) return;
+
+    localStorage.setItem(localStorageKey, JSON.stringify(parsedFilters));
+    
+
+
+};
+
+
+function initForm() {
+    let inputData = localStorage.getItem(localStorageKey);
+  
+    if (inputData) {
+      let parsedInputData = JSON.parse(inputData);
+  
+      Object.entries(parsedInputData).forEach(([name, value]) => {
+        formEl.elements[name].value = value;
+      });
+    }
+}
+
+
+
+
+
+
+
+
+
 
